4001c1b9a2580bfc8440e4f36ab382b3742e23b5
P0plata <guilherme.ja514@gmail.com>
2023-11-08 10:09:36 +0000

    v2-aut
    coreçao do merge e do bri.

--- tester.c
+++ tester.c
@@ -12,18 +12,23 @@ int main(){
     ImageInit();
     Image img = ImageLoad("bri.pgm");
     Image img2 = ImageLoad("bricopy.pgm");
+    Image img3 = ImageLoad("original.pgm");
     uint8 x;
     uint8 x1;
     int o;
-    for(long u;u<ImageHeight(img);u++){
-        for (long y;y<ImageWidth(img);y++) {
-            if(ImageGetPixel(img, y,u) != ImageGetPixel(img2,y,u)){
-                printf("%d|%d||%d|%d\n",y,u,ImageGetPixel(img, y,u),ImageGetPixel(img2,y,u));
+    for(long u=0;u<ImageHeight(img);u++){
+        for (long y=0;y<ImageWidth(img);y++) {
+            if(!(ImageGetPixel(img, y,u) == ImageGetPixel(img2,y,u))){
+                printf("%d|%d||1%d|2%d/o%d\n",u,y,ImageGetPixel(img, y,u),
+                ImageGetPixel(img2,y,u),ImageGetPixel(img3, y,u));
             }
         }
     }
+    printf("%d",(uint8)(ImageGetPixel(img3, 150,2)*0.33));
+    printf("\\%d",(ImageGetPixel(img, 150,2))==ImageGetPixel(img2, 150,2));
     ImageDestroy(&img);
     ImageDestroy(&img2);
+    ImageDestroy(&img3);
     printf("f");
 }
 //bri copy
